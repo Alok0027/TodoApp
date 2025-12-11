a7e9cb1ed176ab6e281e6a05f437b4b8e1a225aa
Alok Mishra <alokmishra.it.2003@gmail.com>
2025-12-11 23:44:43 +0530

    Initial commit - Todo App

---
+++ config/database.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+require('dotenv').config();
+
+const connectDB = () => {
+    mongoose.connect(process.env.DATABASE_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("Database is connected successfully"))
+    .catch( (error) => {
+        console.log("Database sucked");
+        console.error(error.message);
+        process.exit(1);
+        } );
+    }
+
+    module.exports = connectDB;
---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+
+//load config from env files
+require("dotenv").config();
+const PORT = process.env.PORT || 3000;
+
+//middleware to parse json request body
+app.use(express.json());
+
+//import routes for TODO Api
+const todoRoutes=require("./routes/todos");
+//mount the todo APIsroutes
+app.use("/api/v1", todoRoutes);
+
+//start the server
+app.listen(PORT, ()=>{
+    console.log('Server is running on ${PORT}');
+})
+
+//connect to the database
+const dbConnect = require("./config/database");
+dbConnect();
+
+//default route
+app.get("/", (req,res) => {
+    res.send("Welcome Alok to 2026");
+})
